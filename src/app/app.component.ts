ce944a72c0e64d6654197450ca7c13dce1469d8b
Lex <100075440+lexkh@users.noreply.github.com>
2023-11-13 23:08:47 +0300

    Final first version for deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'burgers';
+  title = "Tasty burgers";
+
+  currency = '$'
+
+  form = this.fb.group({
+    order: ["", Validators.required],
+    name: ["", Validators.required],
+    phone: ["", Validators.required]
+  })
+
+  productsData: any;
+
+
+  constructor(private fb: FormBuilder, private appService: AppService) {
+  }
+
+  ngOnInit(){
+    this.appService.getData().subscribe(data => this.productsData = data);
+  }
+
+  scrollTo(target: HTMLElement, burger?: any) {
+    target.scrollIntoView({behavior: "smooth"});
+    if (burger) {
+      this.form.patchValue({order: burger.title + ' (' + burger.price + ' ' + this.currency + ')'});
+    }
+  }
+
+  confirmOrder() {
+    if (this.form.valid) {
+
+      this.appService.sendOrder(this.form.value)
+        .subscribe(
+          {
+            next: (response:any) => {
+              alert("Спасибо за заказ! Мы скоро свяжемся с Вами!");
+              this.form.reset(response.message);
+             },
+            error: (response) => {
+              alert(response.error.message);
+            },
+          }
+        );
+    }
+  }
+
+  changeCurrency() {
+    let newCurrency = "$";
+    let coefficient = 1;
+
+    if (this.currency === "$") {
+      newCurrency = "₽";
+      coefficient = 95;
+    } else if (this.currency === "₽") {
+      newCurrency = "BYN";
+      coefficient = 3;
+    } else if (this.currency === 'BYN') {
+      newCurrency = '€';
+      coefficient = 0.9;
+    } else if (this.currency === '€') {
+      newCurrency = '¥';
+      coefficient = 150;
+    }
+    this.currency = newCurrency;
+
+    this.productsData.forEach((item: any) => {
+      item.price = +(item.basePrice * coefficient).toFixed(1);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {NgOptimizedImage} from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        NgOptimizedImage,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
